11fcb4f0d52050e6da4b1426d6a04a5394cafb0b
jbreit <jason.breitigan@gmail.com>
2023-10-15 10:27:41 -0400

    ci: edit removed prettier execution code via comment

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   'src/**/*.{cjs,mjs,js,jsx,cts,mts,ts,tsx,vue}': ['npm run lint:fix'],
-  'src/**/*.{cjs,mjs,js,jsx,cts,mts,ts,tsx,json,css,scss,vue}': [
-    'prettier --write',
-  ],
+  // 'src/**/*.{cjs,mjs,js,jsx,cts,mts,ts,tsx,json,css,scss,vue}': [
+  //   'prettier --write',
+  // ],
   '*.md': ['npm run mdlint:fix'],
 };
