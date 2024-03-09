50098aa2aaedf2324d7cdcb12bc54f3a73e88d70
jbreit <jason.breitigan@gmail.com>
2024-03-09 00:42:40 -0500

    build(cz.adapter.cjs): edit code style
    Edited code style due to line length

--- tools/cz.adapter.cjs
+++ tools/cz.adapter.cjs
@@ -1,3 +1,4 @@
 exports.prompter = async (inquirerIns, commit) => {
-  (await import('@commitlint/cz-commitlint')).prompter(inquirerIns, commit)
+  (await import('@commitlint/cz-commitlint'))
+    .prompter(inquirerIns, commit);
 }
